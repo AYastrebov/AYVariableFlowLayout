c07cd67da73df516024da67ecd4a1f0a4aeba1c8
Andrey Yastrebov <andrey.yastrebov@corp.mail.ru>
2016-12-16 17:02:41 +0300

    - added layout

---
+++ AYVariableFlowLayout/AYVariableFlowLayout.h
@@ -0,0 +1,18 @@
+//
+//  AYVariableFlowLayout.h
+//  AYVariableFlowLayout
+//
+//  Created by Andrey Yastrebov on 16.12.16.
+//  Copyright © 2016 Andrey Yastrebov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol AYVariableDelegateFlowLayout <NSObject>
+
+
+@end
+
+@interface AYVariableFlowLayout : UICollectionViewLayout
+
+@end
---
+++ AYVariableFlowLayout/AYVariableFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  AYVariableFlowLayout.m
+//  AYVariableFlowLayout
+//
+//  Created by Andrey Yastrebov on 16.12.16.
+//  Copyright © 2016 Andrey Yastrebov. All rights reserved.
+//
+
+#import "AYVariableFlowLayout.h"
+
+@implementation AYVariableFlowLayout
+
+@end
