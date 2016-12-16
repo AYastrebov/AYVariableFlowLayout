38b02c731257e72a94ef620ddea6da380f38d0df
Andrey Yastrebov <andrey.yastrebov@corp.mail.ru>
2016-12-16 16:48:44 +0300

    Initial Commit

---
+++ AYVariableFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AYVariableFlowLayout
+//
+//  Created by Andrey Yastrebov on 16.12.16.
+//  Copyright © 2016 Andrey Yastrebov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
